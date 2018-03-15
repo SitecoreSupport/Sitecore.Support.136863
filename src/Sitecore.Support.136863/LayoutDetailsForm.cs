4b7cd30809bf1fb9e0c9a243e14566a7d437a21b
Ivan Sheyenko <ivs@sitecore.net>
2018-03-15 18:30:57 -0400

    fix

---
+++ src/Sitecore.Support.136863/LayoutDetailsForm.cs
@@ -0,0 +1,33 @@
+﻿using Sitecore.Xml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+
+namespace Sitecore.Support.Shell.Applications.ContentManager.Dialogs.LayoutDetails
+{
+    public class LayoutDetailsForm : Sitecore.Shell.Applications.ContentManager.Dialogs.LayoutDetails.LayoutDetailsForm
+    {
+        public override string Layout
+        {
+            get
+            {
+                return base.Layout;
+            }
+
+            set
+            {
+                base.Layout = value;
+                if (!string.IsNullOrEmpty(this.LayoutDelta))
+                {
+                    XmlDocument xmlDocument = XmlUtil.LoadXml(this.LayoutDelta);
+                    if (!xmlDocument.Schemas.Contains("xmlns:p=\"p\""))
+                    {
+                        this.FinalLayout = this.LayoutDelta;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Sitecore.Support.136863/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.136863")]
+[assembly: AssemblyProduct("Sitecore.Support.136863")]
+[assembly: ComVisible(false)]
